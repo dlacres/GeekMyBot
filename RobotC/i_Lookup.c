0d18371487c349ec684b65bfb4bead6fb691f8f1
dlacres <dlacres@gmail.com>
2014-12-08 19:59:25 -0600

    Fix negative numbers

--- RobotC/i_Lookup.c
+++ RobotC/i_Lookup.c
@@ -15,22 +15,26 @@ int i,num,den;
 
 int Lookup1(int in){
 	int out = 0;
+	int inAbs;
 	int s=sizeof(table_1)/sizeof(table_1[0])-1;
+
+	inAbs=abs(in);
 
 	// Find the x region we are in
 	for (i=0; i<s-1; ++i){
-		if (table_1[i][x]<=in && table_1[i+1][x]>in)
+		if (table_1[i][x]<=inAbs && table_1[i+1][x]>inAbs)
 			break;
 	}
 	num=table_1[i+1][y]-table_1[i][y];
 	den=table_1[i+1][x]-table_1[i][x];
-	out=(num*in)/den + table_1[i][y] - (num*table_1[i][x])/den;
+	out=(num*inAbs)/den + table_1[i][y] - (num*table_1[i][x])/den;
 	//out = (int)((float)(table_1[i+1][y]-table_1[i][y])/((float)(table_1[i+1][x]-table_1[i][x])))*(float)in;
 
+	//if (in<0) out=-out;
 	return(out);
 }
 //----------Test Code------------//
-//#define TEST_DEBUG //-------------------------------------------------
+#define TEST_DEBUG //-------------------------------------------------
 #ifdef TEST_DEBUG
 
 #pragma DebuggerWindows ("debugStream")// Bring up the debug stream window
@@ -39,9 +43,10 @@ int out;
 task main(){
 	writeDebugStreamLine("in,out,i,num,den");
 
-	for (int in=1;in<100;in++){
+	for (int in=-150;in<150;in++){
 		out=Lookup1(in);
 		writeDebugStreamLine("%3i,%3i,%3i,%3i,%3i",in,out,i,num,den);
+		wait1Msec(10);
 	}
 }//*/
 #endif
