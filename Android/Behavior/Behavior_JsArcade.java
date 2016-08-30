ba9921401474ab0aeabb4f0a329ad0bdcb77ff0a
unknown <dlacres@gmail.com>
2016-08-29 19:49:27 -0500

    Change Joystick names, add tank drive

---
+++ Android/Behavior/Behavior_JsArcade.java
@@ -0,0 +1,52 @@
+package com.qualcomm.ftcrobotcontroller.geekmybot;
+
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David Lempia on 7/20/2015.
+ *
+ * Use the left joystick in arcade mode to drive the robot
+ * X is the direction
+ * Y is the throttle
+ */
+public class Behavior_JsArcade implements Behavior {
+    // Change the name of this behavior (for diagnostics)
+    String behaviorName="JS Basic";
+
+    // Add code for onEntry
+    public void onEntry(){
+
+    }
+    // Add code for onExit
+    public void onExit(){
+
+    }
+    // Add code for onLoop
+    public void onLoop(){
+        // Drive calculations using Joystick inputs;
+        float throttle = gd.jsLeftY/2.0f;
+        float direction = gd.jsLeftX/3.0f;
+        float right =  throttle - direction;
+        float left = throttle + direction;
+        gd.rightCmd = Range.clip(right, -1, 1);
+        gd.leftCmd = Range.clip(left, -1, 1);
+    }
+    GlobalData gd;
+    List <Event> eventList;
+
+    public Behavior_JsArcade(GlobalData gd, String behaviorName){
+        this.gd = gd;
+        eventList=new ArrayList<Event>();
+        this.behaviorName=behaviorName;
+    }
+    public String getName(){return(this.behaviorName);}
+    public List<Event> getEventList(){
+        return(eventList);
+    }
+    public void addEvent(Event newEv){
+        eventList.add(newEv);
+    }
+}
---
+++ Android/Behavior/Behavior_JsTank.java
@@ -0,0 +1,48 @@
+package com.qualcomm.ftcrobotcontroller.geekmybot;
+
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David Lempia on 7/20/2015.
+ *
+ * Use the joystick in tank mode to drive the robot
+ * Left Y is the left motor
+ * Right Y is the right motor
+ */
+public class Behavior_JsTank implements Behavior {
+    // Change the name of this behavior (for diagnostics)
+    String behaviorName="JS Basic";
+
+    // Add code for onEntry
+    public void onEntry(){
+
+    }
+    // Add code for onExit
+    public void onExit(){
+
+    }
+    // Add code for onLoop
+    public void onLoop(){
+        // Drive calculations using Joystick inputs;
+        gd.rightCmd = gd.jsRightY;
+        gd.leftCmd = gd.jsLeftY;
+    }
+    GlobalData gd;
+    List <Event> eventList;
+
+    public Behavior_JsTank(GlobalData gd, String behaviorName){
+        this.gd = gd;
+        eventList=new ArrayList<Event>();
+        this.behaviorName=behaviorName;
+    }
+    public String getName(){return(this.behaviorName);}
+    public List<Event> getEventList(){
+        return(eventList);
+    }
+    public void addEvent(Event newEv){
+        eventList.add(newEv);
+    }
+}
